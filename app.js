f86e9457a1b983807405094d4b8df99cf7cbf2b9
maheshbasnet089 <basnetmanish089@gmail.com>
2024-09-30 11:49:43 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require("./model/index")
+const express = require("express")
+const app = express()
+app.set('view engine','ejs')
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs")
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+
+app.listen(3000,()=>{
+    console.log("project has started")
+})
